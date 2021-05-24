5a35a67fa66470bf0ccc91a89d805d673b4e864e
semanovik <novikreef@gmail.com>
2021-05-24 21:10:51 +0200

    third homework TC#1_only_body

--- module_3/test_sample.py
+++ module_3/test_sample.py
@@ -3,34 +3,36 @@ from selenium import webdriver
 login_page_link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
 
 # Data
-enrty_to_login_page_button_locator = '[id="login_link"]'
-login_email_locator = '[name="login-username"]'
+login_email_locator = '[id="id_login-username"]'
 login_password_locator = '[name="login-password"]'
 login_button_locator = '[name="login_submit"]'
+welcome_message = '[class="alertinner wicon"]'
 logout_button_locator = '[id="logout_link"]'
+enrty_to_login_page_button_locator = '[id="login_link"]'
+
 
 # Arrange
 browser = webdriver.Chrome()
 browser.implicitly_wait(5)
 browser.get(login_page_link)
 
+input_login = browser.find_element_by_css_selector(login_email_locator)
+input_login.clear()
 
-login_email_input = browser.find_element_by_css_selector(login_email_locator)
-login_email_input.clear()
-
-login_password_input = browser.find_element_by_css_selector(login_password_locator)
-login_password_input.clear()
+input_password = browser.find_element_by_css_selector(login_password_locator)
+input_login.clear()
 
 login_button = browser.find_element_by_css_selector(login_button_locator)
 
-
 # Act
-login_email_input.send_keys('semasema@gmail.com')
-login_password_input.send_keys('328225328225')
+input_login.send_keys('semasema@gmail.com')
+input_password.send_keys('328225328225')
 login_button.click()
-browser.find_element_by_css_selector('[class="alertinner wicon"]')
+browser.find_element_by_css_selector(welcome_message)
 browser.find_element_by_css_selector(logout_button_locator).click()
 browser.find_element_by_css_selector(enrty_to_login_page_button_locator).click()
+
+print(browser.current_url)
 
 
 
