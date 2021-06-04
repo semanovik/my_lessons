64c4431375cd6a7b39209e0053ae64a0ffa109dd
semanovik <novikreef@gmail.com>
2021-06-04 18:37:30 +0200

    Fourth homework

--- module_4/part_1/test_items.py
+++ module_4/part_1/test_items.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
+
 def test_guest_should_see_login_link(browser):
     # Arrange
     browser.get(link)
