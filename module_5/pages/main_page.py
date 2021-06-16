2ceff8220e8a667135bec3333f1510e9eaf498c3
Sema Novik <novikreef@gmail.com>
2021-06-16 19:13:40 +0200

    Commit for fifth homework, added some for BasePage, added new method 'should_be_login_link' in main_page

--- module_5/pages/main_page.py
+++ module_5/pages/main_page.py
@@ -1,7 +1,12 @@
-class BasePage():
-    def __init__(self, browser, url):
-        self.browser = browser
-        self.url = url
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
 
-    def open(self):
-        self.browser.get(self.url)
+
+class MainPage(BasePage):
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
--- module_5/test_main_page.py
+++ module_5/test_main_page.py
@@ -1,7 +1,16 @@
+from .pages.main_page import MainPage
+
 link = "http://selenium1py.pythonanywhere.com/"
+
 
 class TestMainPage:
     def test_guest_can_go_to_login_page(self, browser):
-        browser.get(link)
-        login_link = browser.find_element_by_css_selector("#login_link")
-        login_link.click()
+        page = MainPage(browser,
+                        link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page.open()  # открываем страницу
+        page.go_to_login_page()  # выполняем метод страницы - переходим на страницу логина
+
+    def test_guest_should_see_login_link(self, browser):
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
