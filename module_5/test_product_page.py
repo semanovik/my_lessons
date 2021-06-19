e4b1027fd2b8a15dbe9960b1ea29dc6684402929
Sema Novik <novikreef@gmail.com>
2021-06-19 10:28:08 +0200

    Added "@pytest.mark.parametrize" fixture with xfail marks for test

--- module_5/test_product_page.py
+++ module_5/test_product_page.py
@@ -1,34 +1,29 @@
 import time
-
+import pytest
 from .pages.product_page import ProductPage
-
-link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
 
 
 class TestProductPage:
 
-    def test_guest_can_add_product_to_basket(self, browser):
+    @pytest.mark.parametrize('promo_offer',
+                             ["offer0", "offer1", "offer2", "offer3", "offer4", "offer5", "offer6",
+                              pytest.param("offer7", marks=pytest.mark.xfail), "offer8", "offer9"])
+    def test_guest_can_add_product_to_basket(self, browser, promo_offer):
         # Arrange
+        link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo={promo_offer}'
         page = ProductPage(browser, link)
         page.open()
 
         # Act
-        page.should_be_before_product_page()    # Проверяем, что все нужные элементы есть на странце
-        book_title = page.get_book_title_before()   # Запоминаем название книги до добавления
-        book_price = page.get_book_price_before()   # Запоминаем цену книги
-        page.add_to_shopping_cart()   # Добавляем в корзину
-        page.solve_quiz_and_get_code()    # Решаем уравнение
+        page.should_be_before_product_page()  # Проверяем, что все нужные элементы есть на странце
+        book_title = page.get_book_title_before()  # Запоминаем название книги до добавления
+        book_price = page.get_book_price_before()  # Запоминаем цену книги
+        page.add_to_shopping_cart()  # Добавляем в корзину
+        page.solve_quiz_and_get_code()  # Решаем уравнение
 
         # Assert
-        page.should_be_after_product_page()    # Проверяем, что все нужные элементы есть на странице после добавления
-        assert book_title in page.get_book_title_after(), 'Wrong book was added'
-        assert book_price in page.get_cart_amount(), 'Wrong cart amount'
-
-
-
-
-
-
-
-
-
+        page.should_be_after_product_page()  # Проверяем, что все нужные элементы есть на странице после добавления
+        assert book_title == page.get_book_title_after(), f'Wrong book was added: selected book: {book_title}, in ' \
+                                                          f'message: {page.get_book_title_after()} '
+        assert book_price in page.get_cart_amount(), f'Wrong cart amount:price of selected book: {book_price}, in ' \
+                                                     f'cart after: {page.get_cart_amount()} '
